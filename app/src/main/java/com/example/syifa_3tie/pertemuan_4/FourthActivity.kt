3b4886f6ed8275a325326973fc2f103130c3bccc
Syifa <syifa24ti@mahasiswa.pcr.ac.id>
2026-09-24 15:02:12 +0700

    Fix tombol kembali di FourthActivity

--- app/src/main/java/com/example/syifa_3tie/pertemuan_4/FourthActivity.kt
+++ app/src/main/java/com/example/syifa_3tie/pertemuan_4/FourthActivity.kt
@@ -32,8 +32,9 @@ class FourthActivity : AppCompatActivity() {
         Log.e("Data Intent","Nama: $name , Usia: $age, Asal: $from")
 
         binding.btnKembali.setOnClickListener {
-            val intent = Intent(  this@FourthActivity, MainActivity::class.java)
-            startActivity(intent)
+            //val intent = Intent(  this@FourthActivity, MainActivity::class.java)
+            //startActivity(intent)
+            finish()
         }
     }
 }
